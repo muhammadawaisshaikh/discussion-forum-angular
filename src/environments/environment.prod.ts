1e5d145091ea0aa7694f9e085b6f8d34ed543555
Muhammad Awais <awaisshaikh94@gmail.com>
2021-04-19 15:48:10 +0500

    init firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBYe2NQ9S3g5Xk4CPpZoiHMsDOGMwS72JU",
+    authDomain: "discussion-forum-2b7ea.firebaseapp.com",
+    projectId: "discussion-forum-2b7ea",
+    storageBucket: "discussion-forum-2b7ea.appspot.com",
+    messagingSenderId: "104972724546",
+    appId: "1:104972724546:web:661b10dc6f1038cf8b40db",
+    measurementId: "G-LXKBW53P75"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBYe2NQ9S3g5Xk4CPpZoiHMsDOGMwS72JU",
+    authDomain: "discussion-forum-2b7ea.firebaseapp.com",
+    projectId: "discussion-forum-2b7ea",
+    storageBucket: "discussion-forum-2b7ea.appspot.com",
+    messagingSenderId: "104972724546",
+    appId: "1:104972724546:web:661b10dc6f1038cf8b40db",
+    measurementId: "G-LXKBW53P75"
+  }
 };
 
 /*
